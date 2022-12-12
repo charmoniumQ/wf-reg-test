1881b302b85f16a21953c8c3687218a01a85ba7a
Samuel Grayson <sam@samgrayson.me>
2022-12-12 12:45:37 -0600

    --amend

--- parsl_configs/azure.py
+++ parsl_configs/azure.py
@@ -2,16 +2,17 @@ import parsl
 import os
 
 parsl.load(parsl.config.Config(
-    executors=parsl.executors.HighThroughputExecutor(
-        provider=parsl.providers.AdHocProvider(
-            channels=[
-                parsl.channels.SSHChannel(hostname)
-                for hostname in os.environ["PARSL_WORKERS"].split(",")
-            ],
-            # worker_init="source $HOME/spack/activate.sh",
-            worker_init="source $HOME/spack/share/spack/setup-env.sh\nspack env activate wf-reg-test\n",
-            parallelism=1,
+    executors=[
+        parsl.executors.HighThroughputExecutor(
+            provider=parsl.providers.AdHocProvider(
+                channels=[
+                    parsl.channels.SSHChannel(hostname)
+                    for hostname in os.environ["PARSL_WORKERS"].split(",")
+                ],
+                worker_init="source $HOME/spack/activate.sh",
+                parallelism=1,
+            ),
+            max_workers=parallelism,
         ),
-        max_workers=parallelism,
-    ),
+    ],
 ))
