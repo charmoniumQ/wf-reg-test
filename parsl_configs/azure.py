052a233a2e112263e46e9408bf43dbccaf3284e9
Samuel Grayson <sam@samgrayson.me>
2022-12-12 23:53:39 -0600

    --ammend

--- parsl_configs/azure.py
+++ parsl_configs/azure.py
@@ -1,12 +1,14 @@
 import parsl
 import os
 
+parallelisum = 1
+
 parsl.load(parsl.config.Config(
     executors=[
         parsl.executors.HighThroughputExecutor(
             provider=parsl.providers.AdHocProvider(
                 channels=[
-                    parsl.channels.SSHChannel(hostname)
+                    parsl.channels.SSHChannel(hostname, script_dir="/home/azureuser")
                     for hostname in os.environ["PARSL_WORKERS"].split(",")
                 ],
                 worker_init="source $HOME/spack/activate.sh",
@@ -16,3 +18,14 @@ parsl.load(parsl.config.Config(
         ),
     ],
 ))
+
+@parsl.python_app
+def f(x):
+    return x**2
+print(f(3).result())
+
+@parsl.python_app
+def g():
+    import platform
+    return platform.node()
+print(g().result())
