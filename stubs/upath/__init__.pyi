d107b8836ef60bd43df2cad6036a57b7e6fee340
Samuel Grayson <sam@samgrayson.me>
2022-12-19 15:38:01 -0600

    --amend

--- stubs/upath/__init__.pyi
+++ stubs/upath/__init__.pyi
@@ -1,10 +1,14 @@
 import pathlib
+from typing import Any
+import urllib.parse
+
 import fsspec
 
 
 class UPath(pathlib.PurePosixPath):
-    def __init__(self, url: str) -> None: ...
+    def __init__(self, url: str, **extra: Any) -> None: ...
     fs: fsspec.spec.AbstractFileSystem
     path: str
+    _url: urllib.parse.ParseResult
 
     def __div__(self, other: str) -> UPath: ...
