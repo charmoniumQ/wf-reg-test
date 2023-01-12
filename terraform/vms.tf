1a18b6cdd71f2746b0ff339eb7eab0bca6342b01
Samuel Grayson <sam@samgrayson.me>
2023-01-12 14:32:26 -0600

    Fix rebase conflicts

--- terraform/vms.tf
+++ terraform/vms.tf
@@ -179,3 +179,74 @@ resource azurerm_role_assignment worker-data {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = azurerm_linux_virtual_machine.worker[count.index].identity[0].principal_id
 }
+
+# #############################################
+# # Builder
+# #############################################
+
+# resource "azurerm_public_ip" "builder_ip" {
+#   name                = "builder"
+#   resource_group_name = azurerm_resource_group.default.name
+#   location            = azurerm_resource_group.default.location
+#   allocation_method   = "Dynamic"
+# }
+
+
+# resource "azurerm_network_interface" "builder_nic" {
+#   name                = "builder_nic"
+#   location            = azurerm_resource_group.default.location
+#   resource_group_name = azurerm_resource_group.default.name
+
+#   ip_configuration {
+#     name                          = "builder"
+#     subnet_id                     = azurerm_subnet.default.id
+#     private_ip_address_allocation = "Dynamic"
+#     public_ip_address_id          = azurerm_public_ip.builder_ip.id
+#   }
+# }
+
+# resource "azurerm_network_interface_security_group_association" "builder" {
+#   network_interface_id      = azurerm_network_interface.builder_nic.id
+#   network_security_group_id = azurerm_network_security_group.sshable_nsg.id
+# }
+
+# resource "azurerm_linux_virtual_machine" "builder" {
+#   name                  = "builder"
+#   location              = azurerm_resource_group.default.location
+#   resource_group_name   = azurerm_resource_group.default.name
+#   size                  = var.builder_vm_size
+#   admin_username        = var.username
+#   network_interface_ids = [azurerm_network_interface.builder_nic.id]
+#   disable_password_authentication = true
+#   admin_ssh_key {
+#     username   = var.username
+#     public_key = tls_private_key.developer.public_key_openssh
+#   }
+#   source_image_reference {
+#     publisher = var.vm_image.publisher
+#     offer     = var.vm_image.offer
+#     sku       = var.vm_image.sku
+#     version   = var.vm_image.version
+#   }
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "StandardSSD_LRS"
+#     disk_size_gb         = var.os_disk_size_gb
+#   }
+#   identity {
+#     type = "SystemAssigned"
+#   }
+#   # connection {
+#   #   type        = "ssh"
+#   #     user        = var.username
+#   #     host        = azurerm_linux_virtual_machine.builder.public_ip_address
+#   #     private_key = tls_private_key.developer.private_key_openssh
+#   # }
+#   # provisioner "remote-exec" {
+#   #     script = "../spack/setup_env.sh"
+#   # }
+# }
+
+# output "builder_ip" {
+#   value = azurerm_linux_virtual_machine.builder.public_ip_address
+# }
